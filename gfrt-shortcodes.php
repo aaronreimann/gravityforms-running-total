b619921c32d208b621b2e9f000c6c05fdaa53235
Aaron Reimann <aaron.reimann@gmail.com>
2016-02-19 09:47:23 -0500

    Adds debug option to the shortcode

--- gfrt-shortcodes.php
+++ gfrt-shortcodes.php
@@ -9,12 +9,13 @@ class GFRT_Shortcodes {
 	}
 
 	/*
-	Usage: [gfrt id="1" field="3"]
+	Usage: [gfrt id="1" field="3" debug="0"]
 	*/
 	function total( $atts ) {
 		$form_id = 1; // Gets overwritten, but keeps PhpStorm from being angry.
 		$form_id = $atts['id'];
-		$total = 0;
+		$debug   = $atts['debug'];
+		$total   = 0;
 
 		// From Chris Hajer April 15, 2014 to return more than 30 entries (the default)
 		// Grabs all of the entries based on the $form_id
@@ -23,8 +24,13 @@ class GFRT_Shortcodes {
 		// loops through each form and grabs the "field" ID number based on what is submitted to the shortcode
 		foreach ( $entries as $entry ) {
 
+			// added this so a person can get a list of the data that is being submitted without looking at the DB
+			if ( ( $debug == "true" ) OR ( $debug > 0 ) ) {
+				print_r( $entry );
+			}
+
 			// strips the $
-			$single_amount = str_replace('$', '', $entry[$atts[field]]);
+			$single_amount = str_replace( '$', '', $entry[ $atts[ field ] ] );
 
 			// adds each value to to the total
 			$total += $single_amount;
@@ -37,15 +43,16 @@ class GFRT_Shortcodes {
 		$formatted_number = number_format_i18n( $total, 2 );
 
 		// appends currency symbol
-		$dollar_amount = $cs.$formatted_number;
+		$dollar_amount = $cs . $formatted_number;
 
 		return $dollar_amount;
 	}
 
 	function currency_symbol() {
-		setlocale(LC_MONETARY, get_locale());
+		setlocale( LC_MONETARY, get_locale() );
 		$local_currency = localeconv();
-		$cs = $local_currency[currency_symbol];
+		$cs             = $local_currency[ currency_symbol ];
+
 		return $cs;
 	}
 }
